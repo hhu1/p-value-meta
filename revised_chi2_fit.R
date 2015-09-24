f233f01f18a0c9a39a3c530b8334796a2a38a95f
hhu1 <haohupku@gmail.com>
2015-09-24 14:52:23 -0500

    Update revised_chi2_fit.R
    Added Brown's method for p-value meta-analysis. Call bronw() function.

--- revised_chi2_fit.R
+++ revised_chi2_fit.R
@@ -1,3 +1,34 @@
+brown = function(m) {
+  # implementation of method 4 (Brown method) in: Accuracy Evaluation of the Unified P-Value from Combining Correlated P-Values
+  # the input is a matrix, whose columns correspond to interested phenotypes, and rows corresponds to genes. The values in the matrix are the p-value of association between the gene and each phenotype, based on any rare-variant association test. 
+  
+  m[is.na(m)] = 1
+  m[m==0] = 1e-20
+  
+  num_genes = nrow(m)
+  num_pts = ncol(m)
+  
+  cor_m = cor(log(m))
+  cov_m = 3.263 * cor_m + 0.710 * (cor_m^2) + 0.027* (cor_m^3)
+  
+  sum_term = 0 # this is the common sum term in c and f calculation
+  for (i in 1:(num_pts-1)) {
+    for (j in (i+1):num_pts) {
+      sum_term = sum_term + cov_m[i,j]
+    } 
+  }
+    
+  c = (2* num_pts + sum_term) / (2* num_pts)
+  f = 4 * num_pts^2 / (2*num_pts + sum_term)
+  
+  ps = numeric()
+  for (i in 1:num_genes) {
+    tau = sum(-2* log(m[i,]))
+    ps[i] = pchisq(tau/c, f, lower.tail=F)
+  }
+  return(ps) # returning a vector of p-values, one for each gene
+}
+
 rvat_chi2 = function(m) {
   # the input is a matrix, whose columns correspond to interested phenotypes, and rows corresponds to genes. The values in the matrix are the p-value of association between the gene and each phenotype, based on any rare-variant association test. 
   m[is.na(m)] = 1
@@ -25,4 +56,5 @@ rvat_chi2 = function(m) {
 
 # example: calculate the meta-analysis p-values for 300 genes across 8 traits. 
 m =matrix(runif(300*8), nrow=300)
-ps = rvat_chi2(m)
+p1 = rvat_chi2(m)
+p2 = brown(m)
